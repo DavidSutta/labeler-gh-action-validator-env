8af1c9f9333d9a1df660cc941cc61859982c94ca
David Sutta <david.sutta@cloudera.com>
2024-10-01 12:56:58 +0000

    add tsconfig and jest config and base dummy tc

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  clearMocks: true,
+  moduleFileExtensions: ['js', 'ts'],
+  testEnvironment: 'node',
+  testMatch: ['<rootDir>/tests/**/*.test.ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  },
+  verbose: true,
+  rootDir: '/workspaces/labeler-gh-action-validator-env',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/labeler_v5/$1'
+  }
+}
